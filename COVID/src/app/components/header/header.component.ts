bcecd8e74264c3b62099f3b08561126ede4b470e
Psaruklily <psarukli96@gmail.com>
2020-09-17 20:30:32 +0300

    Test property and method

--- COVID/src/app/components/header/header.component.ts
+++ COVID/src/app/components/header/header.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  name: string;
+  speed: number;
+  model: string;
+  colors: Colors;
+  options: string[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.name = 'Audi';
+    this.speed = 235;
+    this.model = 'RS8';
+    this.colors = {
+      car: 'white',
+      salon: 'black',
+      wheels: 'silver'
+    };
+    this.options = ['ABS', 'Автопілот', 'Авто піркінг'];
   }
 
+}
+
+interface Colors {
+  car: string;
+  salon: string;
+  wheels: string;
 }

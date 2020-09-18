2b8a01b16041e667babd8c6ad8c50dc742babfd2
Psaruklily <psarukli96@gmail.com>
2020-09-18 17:11:26 +0300

    HTML and event(click)

--- COVID/src/app/components/header/header.component.ts
+++ COVID/src/app/components/header/header.component.ts
@@ -20,11 +20,46 @@ export class HeaderComponent implements OnInit {
     this.speed = 235;
     this.model = 'RS8';
     this.colors = {
-      car: 'white',
-      salon: 'black',
-      wheels: 'silver'
+      car: 'білий',
+      salon: 'чорний',
+      wheels: 'срібний'
     };
-    this.options = ['ABS', 'Автопілот', 'Авто піркінг'];
+    this.options = ['ABS', 'Автопілот', 'Авто паркінг'];
+  }
+
+  carSelect(carName){
+    if(carName == 'bmw'){
+      this.name = 'BMW';
+      this.speed = 280;
+      this.model = 'M5';
+      this.colors = {
+        car: 'синій',
+        salon: 'білий',
+        wheels: 'срібний'
+      };
+      this.options = ['Система трека', 'Круїз контроль'];
+    } else if(carName == 'audi'){
+      this.name = 'Audi';
+      this.speed = 235;
+      this.model = 'RS8';
+      this.colors = {
+        car: 'білий',
+        salon: 'чорний',
+        wheels: 'срібний'
+      };
+      this.options = ['ABS', 'Автопілот', 'Авто паркінг'];
+    } else {
+      this.name = 'Mercedes';
+      this.speed = 180;
+      this.model = 'C180';
+      this.colors = {
+        car: 'білий',
+        salon: 'чорний',
+        wheels: 'срібний'
+      };
+      this.options = ['Підігрів сидіння', 'Перегляд фільмів'];
+    }
+  
   }
 
 }

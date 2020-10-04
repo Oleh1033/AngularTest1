46ee8cef3f51374169a54595a905725537369797
Psaruklily <psarukli96@gmail.com>
2020-10-04 15:37:58 +0300

    Usage service in the progect

--- COVID/src/app/app.component.ts
+++ COVID/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [DataService]
 })
-export class AppComponent {
- 
+export class AppComponent implements OnInit {
+
+  name: string;
+  items: string[] = [];
+
+  constructor(private dataService: DataService){}
+
+  addItem(name){
+    this.dataService.addData(name);
+  }
+
+  ngOnInit(){
+    this.items = this.dataService.getData();
+
+  }
+
+
 }
--- COVID/src/app/app.module.ts
+++ COVID/src/app/app.module.ts
@@ -3,17 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { FormsModule }   from '@angular/forms';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

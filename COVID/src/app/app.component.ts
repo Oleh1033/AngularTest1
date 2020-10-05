589cc22c8c993281a82aaabd2b43639f1bf2d686
Psaruklily <psarukli96@gmail.com>
2020-10-05 13:15:31 +0300

    Simple get request to json file

--- COVID/src/app/app.component.ts
+++ COVID/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user'
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
  
+user: User;
+constructor(private http: HttpClient){}
+
+ngOnInit(){
+  this.http.get('assets/user.json').subscribe((data:User) => this.user=data);
+}
 }

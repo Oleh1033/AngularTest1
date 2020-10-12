5050eda9a3fae5af920832554ba4c41079576447
Psaruklily <psarukli96@gmail.com>
2020-10-12 17:37:30 +0300

    Query string parametrs

--- COVID/src/app/item.component.ts
+++ COVID/src/app/item.component.ts
@@ -4,16 +4,29 @@ import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'item-app',
-    template: `<h3>Модель {{id}}</h3>`
+    template: `<h3>Модель {{id}}</h3>
+    <div>Товар: {{product}}</div>
+                <div>Ціна: {{price}}</div>`
 })
 
 export class ItemComponent {
 
 id: number;
-private subscription: Subscription;
+private routeSubscription: Subscription;
+private querySubscription: Subscription;
+product: string;
+price: string;
 
 constructor(private activateRoute: ActivatedRoute){
-    this.subscription = activateRoute.params.subscribe(params => this.id = params['id'])
+    this.routeSubscription = activateRoute.params.subscribe(params => {
+        this.id = params['id'];
+    });
+
+    this.querySubscription = activateRoute.queryParams.subscribe((queryParam: any) => {
+        console.log(queryParam)
+        this.product = queryParam['product'];
+        this.price = queryParam['price'];
+    })
 }
 
 }
